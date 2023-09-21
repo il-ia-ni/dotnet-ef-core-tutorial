8322e981d00d5274fb37f76502be3e603e88f769
Ilia Nikolaenko <niil02@sms-group.com>
2023-09-21 09:32:42 +0200

    Configured Web API services and added db context

---
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Services/ILibraryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DBFirstLibrary;
+
+namespace library_webapi_ef_tutorial.Services
+{
+    public interface ILibraryRepository
+    {
+        IQueryable<Book> ReadBooks();
+        Book ReadBooks(int bookId);
+        void CreateBook(Book book);
+        void UpdateBook(int bookId, Book book_data);
+        void DeleteBook(int bookId);
+    }
+}
---
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Services/LibraryRepository.cs
@@ -0,0 +1,46 @@
+﻿using DBFirstLibrary;
+
+namespace library_webapi_ef_tutorial.Services
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        private readonly libraryef1Context _dbContext;
+
+        public LibraryRepository(libraryef1Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreateBook(Book book)
+        {
+            _dbContext.Books.Add(book);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            _dbContext.Books.Remove(ReadBooks(bookId)); 
+            _dbContext.SaveChanges();
+        }
+
+        public IQueryable<Book> ReadBooks()
+        {
+            return _dbContext.Books;
+        }
+
+        public Book ReadBooks(int bookId)
+        {
+            return _dbContext.Books.SingleOrDefault(book => book.BookId == bookId);
+        }
+
+        public void UpdateBook(int bookId, Book book_data)
+        {
+            var book = ReadBooks(bookId);
+
+            book.Title = book_data.Title;
+            book.Author = book_data.Author;
+            book.PublishedDate = book_data.PublishedDate;
+            _dbContext.SaveChanges();
+        }
+    }
+}
