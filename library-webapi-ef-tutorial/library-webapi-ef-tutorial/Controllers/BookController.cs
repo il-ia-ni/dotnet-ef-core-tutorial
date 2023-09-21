08d64bcf481ab2335718a2a738d5c1a15df992b9
Ilia Nikolaenko <niil02@sms-group.com>
2023-09-21 09:41:06 +0200

    Added Book Controller

---
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using DBFirstLibrary;
+using library_webapi_ef_tutorial.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_webapi_ef_tutorial.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private ILibraryRepository _libRepo;
+
+        public BookController(ILibraryRepository libRepo)
+        {
+            _libRepo = libRepo;
+        }
+
+        [HttpGet]
+        public ActionResult<IQueryable<Book>> GetBooks()
+        {
+            return Ok(_libRepo.ReadBooks());
+        }
+    }
+}
