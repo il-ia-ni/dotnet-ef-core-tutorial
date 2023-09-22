cf8df6cd04eab4c84d948b04c778244d127565e4
Ilia Nikolaenko <niil02@sms-group.com>
2023-09-22 15:41:08 +0200

    techn upd

--- library-webapi-ef-tutorial/library-webapi-ef-tutorial/Program.cs
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Program.cs
@@ -6,12 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    // See https://stackoverflow.com/questions/74246482/system-notsupportedexception-serialization-and-deserialization-of-system-dateo
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<libraryef1Context>(options => options.UseMySql(builder.Configuration.GetConnectionString("Library1"), ServerVersion.Parse(builder.Configuration.GetSection("DBVersions").GetValue<string>("MariaDB"))));
+builder.Services.AddDbContext<libraryef1Context>(options => options.UseMySql(builder.Configuration.GetConnectionString("Library1:MariaDB"), ServerVersion.Parse(builder.Configuration.GetSection("DBVersions").GetValue<string>("MariaDB"))));
+
+//builder.Services.AddHttpClient("PrimaryDBConnection")
+//    .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(5)));
 
 builder.Services.AddTransient<ILibraryRepository, LibraryRepository>();
 
---
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Services/DateOnlyJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace library_webapi_ef_tutorial.Services
+{
+    // See https://stackoverflow.com/questions/74246482/system-notsupportedexception-serialization-and-deserialization-of-system-dateo
+    public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateOnly.FromDateTime(reader.GetDateTime());
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        {
+            var isoDate = value.ToString("O");
+            writer.WriteStringValue(isoDate);
+        }
+    }
+}
