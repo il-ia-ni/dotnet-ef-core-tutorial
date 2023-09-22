e42a879b941c17cfdbe80961f58b544e5eb0ad5d
Ilia Nikolaenko <niil02@sms-group.com>
2023-09-22 15:53:22 +0200

    Added HTTP Methods to BookController

--- library-webapi-ef-tutorial/library-webapi-ef-tutorial/Controllers/BookController.cs
+++ library-webapi-ef-tutorial/library-webapi-ef-tutorial/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using library_webapi_ef_tutorial.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace library_webapi_ef_tutorial.Controllers
 {
@@ -17,9 +18,41 @@ namespace library_webapi_ef_tutorial.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Book>), 200)]
         public ActionResult<IQueryable<Book>> GetBooks()
         {
             return Ok(_libRepo.ReadBooks());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Book), 200)]
+        public ActionResult<IQueryable<Book>> GetBookByID(int id)
+        {
+            return Ok(_libRepo.ReadBooks(id));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ActionResult), 201)]
+        public ActionResult CreateBook([FromBody] Book bookData)
+        {
+            _libRepo.CreateBook(bookData);
+            return CreatedAtAction(nameof(GetBookByID), new { id = bookData.BookId });
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(ActionResult), 200)]
+        public ActionResult UpdateBook(int id, [FromBody] Book bookData)
+        {
+            _libRepo.UpdateBook(id, bookData);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(ActionResult), 200)]
+        public ActionResult DeleteBook(int id)
+        {
+            _libRepo.DeleteBook(id);
+            return Ok();
         }
     }
 }
