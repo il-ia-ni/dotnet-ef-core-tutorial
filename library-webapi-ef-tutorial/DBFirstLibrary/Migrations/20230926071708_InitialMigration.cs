884f9f4f5d4e867f8792e00a0f12b87bf076fe20
Ilia Nikolaenko <niil02@sms-group.com>
2023-09-26 09:18:15 +0200

    Added Initial Migration

---
+++ library-webapi-ef-tutorial/DBFirstLibrary/Migrations/20230926071708_InitialMigration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBFirstLibrary.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf16");
+
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<long>(type: "bigint(20)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false, collation: "utf16_german2_ci")
+                        .Annotation("MySql:CharSet", "utf16"),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    DeathDate = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authors", x => x.AuthorID);
+                })
+                .Annotation("MySql:CharSet", "utf16")
+                .Annotation("Relational:Collation", "utf16_german2_ci");
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    BookID = table.Column<long>(type: "bigint(20)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false, collation: "utf16_german2_ci")
+                        .Annotation("MySql:CharSet", "utf16"),
+                    PublishedDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    AuthorID = table.Column<long>(type: "bigint(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.BookID);
+                    table.ForeignKey(
+                        name: "Books_FK",
+                        column: x => x.AuthorID,
+                        principalTable: "authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf16")
+                .Annotation("Relational:Collation", "utf16_german2_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "Books_FK",
+                table: "books",
+                column: "AuthorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "authors");
+        }
+    }
+}
